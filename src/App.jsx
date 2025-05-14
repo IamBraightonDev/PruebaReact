884e9051e526b013035c1e0389e4849b08ef035d
Braighton Maxwell <braightonfoulman@gmail.com>
2025-05-14 18:45:46 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [contador, setContador] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Mi primer proyecto React</h1>
+      <p>Contador: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Sumar 1</button>
+    </div>
+  )
+}
+
+export default App
